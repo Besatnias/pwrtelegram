a9f4700f33ca25806d7df7adc0955ce290e2577c
Besto <manofthebag@gmail.com>
2017-03-13 13:54:47 -0400

    Update example.js

--- example.js
+++ example.js
@@ -7,10 +7,3 @@ bot = new User({
 bot.sendMessage(237799109, "Well, *hello* there!", {parse_mode: 'markdown'})
    .then(res=>bot.sendMessage({chat_id: '237799109', text: 'The text succeeded, Master!'})
    .catch(err=>console.log(err))
-
-bot.sendMessage({
-  chatId: '',
-  text: 'Hello world!'
-}, (error, ctx) => {
-  console.log(ctx)
-});
